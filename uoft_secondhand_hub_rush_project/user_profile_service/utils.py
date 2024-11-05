df4b68546e39029b0deff1cb1ed9bbb3bb3ce65b
ACHUTHAN MUKUNDAN <achuthanmukundan00@gmail.com>
2024-11-05 16:51:27 -0500

    add helper for adding listing id to user

--- uoft_secondhand_hub_rush_project/user_profile_service/utils.py
+++ uoft_secondhand_hub_rush_project/user_profile_service/utils.py
@@ -20,3 +20,25 @@ def upload_to_users_s3(file, filename):
         current_app.logger.error(f"Failed to upload to S3: {e}")
         return None
 
+def add_listing_id_to_user(user_id, listing_id):
+    dynamodb = boto3.resource(
+        'dynamodb',
+        region_name=current_app.config['AWS_S3_REGION'],
+        aws_access_key_id=current_app.config['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY']
+    )
+
+    user_table = dynamodb.Table(current_app.config['AWS_DB_USERS_TABLE_NAME'])
+
+    try:
+        user_table.update_item(
+            Key={'id': user_id},
+            UpdateExpression="ADD listings :new_listing_id",
+            ExpressionAttributeValues={':new_listing_id': {listing_id}},
+            ConditionExpression="attribute_exists(id)",  # Ensure the user exists
+        )
+        current_app.logger.info(f"Listing ID {listing_id} added to user {user_id}")
+        return True
+    except Exception as e:
+        current_app.logger.error(f"Failed to add listing ID to user: {e}")
+        return False
